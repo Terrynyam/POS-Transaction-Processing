49862b8e765ed737ef79385fbe7179d4aa636048
terrancen <terrancen@afrosoft.co.zw>
2024-06-05 18:25:46 +0200

    Initial Commit

---
+++ src/test/java/com/tenic/transaction_process/TransactionProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenic.transaction_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
